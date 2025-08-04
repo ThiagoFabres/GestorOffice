1884157b656786fc80c623c9e11d6aba8b8fd192
Thiago <thiagotejadafabres@gmail.com>
2025-08-04 15:42:41 -0300

    tela de cadastro adicionada

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="gestor-office.png" type="image/x"-icon>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gestor Office</title>
+</head>
+<body style="background-color: #f3f4f7;">
+
+<?php
+if (!empty($_POST)) {
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+}
+?>
+
+<!-- Login -->
+
+<div class="card login-card">
+    <div class="card-body">
+<div id="card-titulo"> <h1>Login</h1></div>
+<div id="card-sub">Entre com a sua conta.</div>
+<form action="/" method="post">
+    <div style="display:flex; flex-direction:row;" class="mb-3">
+        <div class="input-icon">
+        <i class="bi bi-person"></i>
+        </div>
+        <input type="text" class="form-control" id="username" placeholder="Email" name="username" required>
+    </div>
+    <div style="display:flex; flex-direction:row;" class="mb-3">
+        <div class="input-icon">
+        <i class="bi bi-lock"></i>
+        </div>
+        <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+    </div>
+    <div class="footer">
+    <button type="submit" style="background-color:#5856d6; padding-inline:1.5em; " class="btn btn-primary">Login</button>
+    <a style="text-decoration:underline; color:#5856d6; float:right;">Esqueceu a senha?</a>
+    </div>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
