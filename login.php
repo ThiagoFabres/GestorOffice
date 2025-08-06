6ad9dfb309394a7138f07e457cea6d44ec109f3c
Thiago <thiagotejadafabres@gmail.com>
2025-08-06 08:37:25 -0300

    segurança aumentada

--- login.php
+++ login.php
@@ -3,52 +3,49 @@
 require_once __DIR__ . '/db/base.php';
 require_once __DIR__ . '/db/entities/usuarios.php';
 
-$usuarios = Usuario::read();
+session_start();
 
 if (isset($_POST['email']) && isset($_POST['senha'])) {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    $senhaprotegida = password_hash($senha, PASSWORD_DEFAULT);
 
-    foreach ($usuarios as $usuario) {
-        if ($usuario->email === $email && password_verify($senha, $usuario->senha)) {
-            session_start();
-            $_SESSION['usuario'] = $usuario;
 
-            if ($usuario->cargo === 1) {
-                header('Location: admin/index.php');
-            } else if ($usuario->cargo === 2) {
-                header('Location: gerente/index.php');
-            } else if ($usuario->cargo === 3) {
-                header('Location: dashboard.php');
-            }
-            exit;
-        }
+
+    if ($email && $senha) {
+        $usuario = Usuario::read(null, $email); // busca apenas 1 usuário
+
+        if ($usuario && password_verify($senha, $usuario->senha)) {
+
+    session_regenerate_id(true);
+
+    $_SESSION['usuario'] = $usuario;
+
+    switch ($usuario->cargo) {
+        case 1:
+            header('Location: admin/index.php');
+            break;
+        case 2:
+            header('Location: gestor/index.php');
+            break;
+        case 3:
+            header('Location: dashboard.php');
+            break;
+        default:
+            header('Location: index.php');
+            break;
     }
-
-    $error = "Email ou senha inválidos.";
+    exit;
+} else {
+            $error = "Email ou senha inválidos.";
+        }
+    } else {
+        $error = "Dados inválidos.";
+    }
 } else {
     $error = "Por favor, preencha todos os campos.";
 }
 
-echo $error;
 
-echo 'usuarios:';
-
-    echo "<pre>";
-    print_r($usuarios);
-    echo "</pre>";
-
-
-
-echo 'post:';
-if (!empty($_POST)) {
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-}
-
-echo $senhaprotegida;
 
 
 
