35746e218a6b58dfbc1c5fa122eceef278fc09c5
Thiago <thiagotejadafabres@gmail.com>
2025-08-12 15:58:16 -0300

    sistema de login alterado

--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/db/base.php';
 require_once __DIR__ . '/db/entities/usuarios.php';
+require_once __DIR__ . '/db/entities/empresas.php';
+require_once  __DIR__ . '/db/entities/cargo.php';
 
 session_start();
 
@@ -13,28 +15,35 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
 
     if ($email && $senha) {
         $usuario = Usuario::read(null, $email); // busca apenas 1 usuário
+        
 
-        if ($usuario && password_verify($senha, $usuario->senha)) {
+        if ($usuario && password_verify($senha, $usuario[0]->senha)) {
 
     session_regenerate_id(true);
 
-    $_SESSION['usuario'] = $usuario;
+    $_SESSION['usuario'] = $usuario[0];
 
-    switch ($usuario->cargo) {
+
+    switch ($usuario[0]->cargo) {
         case 1:
             header('Location: admin/index.php');
-            break;
+            break;  
         case 2:
-            header('Location: gestor/index.php');
+            $empresa = Empresa::read($usuario[0]->id_empresa);
+
+           if ($empresa[0]->status == 1) {
+    header('Location: gestor/index.php');
+} else {
+    $error = "Empresa inativa.";
+}
             break;
         case 3:
-            header('Location: dashboard.php');
+            header('Location: usuario/index.php');  
             break;
-        default:
-            header('Location: index.php');
-            break;
+
     }
     exit;
+
 } else {
             $error = "Email ou senha inválidos.";
         }
@@ -45,6 +54,7 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     $error = "Por favor, preencha todos os campos.";
 }
 
+echo $error;
 
 
 
