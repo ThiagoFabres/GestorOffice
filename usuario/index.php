ff88af477b6e4fda66d289bcf3a33905948001ea
Thiago <thiagotejadafabres@gmail.com>
2025-08-20 11:26:49 -0300

    seguranca de consultar/processar feita

--- usuario/index.php
+++ usuario/index.php
@@ -1,9 +1,20 @@
 <?php
 
 require_once __DIR__ . '/../db/entities/usuarios.php';
-
+require_once __DIR__ . '/../db/entities/contas.php';
 
 session_start();
+
+
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario']->cargo != 3) {
+    header('Location: /');
+    exit;
+}
+
+if(isset($_GET['view']) && $_GET['view'] == 'contas'){
+    $titulos = Con01::read(null, $_SESSION['usuario']->id_empresa);
+ }
+
 
 ?>
 
@@ -24,7 +35,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="gestor-office.png" type="image/x-icon">
-    <title>Calendario</title>
+    <title>Gestor Office Control</title>
 </head>
 
 <body id="body" >
@@ -36,9 +47,9 @@ session_start();
         </div>
     <div id="itens-menu">
                         <div class="menu-item">
-            <a href="/usuario/"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-layers"></i></div> Dashboard </a>
+            <a href="index.php"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-layers"></i></div> Dashboard </a>
         </div>
-    
+    <?php if($_SESSION['usuario']->processar == 1) { ?>
         <div class="menu-item accordion" >
 
 <a class="nav-link text-white" data-bs-toggle="collapse" href="#cadastrosMenu" role="button" aria-expanded="false" aria-controls="cadastrosMenu">
@@ -46,26 +57,27 @@ session_start();
       </a>
       <div class="collapse" id="cadastrosMenu">
         <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-3">
-          <li><a href="clientes.php" class="link-light text-decoration-none"><i class="bi bi-person"></i>Cliente/Fornecedor</a></li>
-          <li><a href="bairros.php" class="link-light text-decoration-none"><i class="bi bi-houses"></i>Bairro</a></li>
-          <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-buildings"></i>Cidade</a></li>
-          <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-cash-coin"></i>Tipo Pagamento</a></li>
-          <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-tag"></i>Categoria</a></li>
+          <li><a href="cadastrar.php?cadastro=cliente" class="link-light text-decoration-none"><i class="bi bi-person"></i>Cliente/Fornecedor</a></li>
+          <li><a href="cadastrar.php?cadastro=bairro" class="link-light text-decoration-none"><i class="bi bi-houses"></i>Bairro</a></li>
+          <li><a href="cadastrar.php?cadastro=cidade" class="link-light text-decoration-none"><i class="bi bi-buildings"></i>Cidade</a></li>
+          <li><a href="cadastrar.php?cadastro=pagamento" class="link-light text-decoration-none"><i class="bi bi-cash-coin"></i>Tipo Pagamento</a></li>
+          <li><a href="cadastrar.php?cadastro=categoria" class="link-light text-decoration-none"><i class="bi bi-tag"></i>Categoria</a></li>
           
         </ul>
       </div>
         </div>
+        <?php } ?>
 
         <div class="menu-item">
-            <a href="/usuario/"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-journal-bookmark"></i></div> Plano de Contas </a>
+            <a href="index.php?view=contas"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-journal-bookmark"></i></div> Plano de Contas </a>
         </div>
 
         <div class="menu-item">
-            <a href="/usuario/"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-wallet"></i></div> Contas a Receber </a>
+            <a href="index.php?view=receber"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-wallet"></i></div> Contas a Receber </a>
         </div>
 
         <div class="menu-item">
-            <a href="/usuario/"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-cash-stack"></i></div> Contas a Pagar </a>
+            <a href="index.php?view=pagar"> <div style="padding: 0.5em; align-items:center;"><i class="bi bi-cash-stack"></i></div> Contas a Pagar </a>
         </div>
 
         <div class="menu-item accordion" >
@@ -76,11 +88,11 @@ session_start();
 
         <div class="collapse"  id="registrosMenu">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-3">
-            <li><a href="clientes.php" class="link-light text-decoration-none"><i class="bi bi-list-task"></i>Cadastros</a></li>
-            <li><a href="fornecedores.php" class="link-light text-decoration-none"><i class="bi bi-journal-bookmark"></i>Plano de Contas</a></li>
-            <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-wallet"></i>Contas a Receber</a></li>
-            <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-cash-stack"></i>Contas a Pagar</a></li>
-            <li><a href="produtos.php" class="link-light text-decoration-none"><i class="bi bi-file-earmark-text"></i>DRE</a></li>
+            <li><a href="registros.php?registro=cadastros" class="link-light text-decoration-none"><i class="bi bi-list-task"></i>Cadastros</a></li>
+            <li><a href="registros.php?registro=contas" class="link-light text-decoration-none"><i class="bi bi-journal-bookmark"></i>Plano de Contas</a></li>
+            <li><a href="registros.php?registro=receber" class="link-light text-decoration-none"><i class="bi bi-wallet"></i>Contas a Receber</a></li>
+            <li><a href="registros.php?registro=pagar" class="link-light text-decoration-none"><i class="bi bi-cash-stack"></i>Contas a Pagar</a></li>
+            <li><a href="registros.php?registro=dre" class="link-light text-decoration-none"><i class="bi bi-file-earmark-text"></i>DRE</a></li>
             
             </ul>
         </div>
@@ -121,132 +133,108 @@ session_start();
 
 
     
+<div class="main" id="container">
+            <?php if(isset($_GET['view']) && $_GET['view'] == 'contas') { ?>
+                
 
-            
-            <div class="main" id="container">
+                <div class="botao">
+        <a href="index.php?view=contas&target=titulo&acao=adicionar" class="btn btn-primary btn-sm botao-adm-adicionar">Adicionar titulo</a>
+    </div>
 
+        
+
+
+                        <div class="accordion custom-accordion" id="accordionExample">
+<?php foreach($titulos as $i => $titulo) { 
+    
+        $subtitulos = Con02::read(null, $_SESSION['usuario']->id_empresa, $titulo->id);
+
+    ?>
+    
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="heading<?=$i?>">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?=$i?>" aria-expanded="false" aria-controls="collapse<?=$i?>">
+                <span style="color: #303640; font-size:1.1em; font-weight:500;"> <?php echo $titulo->nome; ?> </span>
+            </button>
+        </h2>
+        <div id="collapse<?=$i?>" class="accordion-collapse collapse" aria-labelledby="heading<?=$i?>" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <div class="inner-accordion">
+                     <a href="index.php?view=contas&target=subtitulo&acao=adicionar&con01id=<?=$titulo->id?>" class="btn btn-primary btn-sm botao-adm-adicionar">Adicionar Subtitulo</a>
+
+                     <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($subtitulos as $subtitulo) { ?>
+                            <tr>
+                                <td><?= $subtitulo->nome ?></td>
+                                
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+</div>
+
+      <?php } ?>
+                
                 
     </div>
 
-
     <!-- Modal -->
-    <div class="modal fade" id="modal_empresa" tabindex="-1" role="dialog"
+    <div class="modal fade" id="modal_titulo" tabindex="-1" role="dialog"
         aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class=" modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Cadastrar</h5>
+                    <?php if(isset($_GET['target'] )&& $_GET['target'] == 'titulo') {
+                        $titulo_modal = 'Título';
+                        $target = 'titulo';
+
+                    } else if(isset($_GET['target']) && $_GET['target'] == 'subtitulo') {
+                        $titulo_modal = 'Subtítulo';
+                        $target = 'subtitulo';
+                    } 
+                     ?>
+                    <h5 class="modal-title" id="exampleModalLongTitle">Novo <?= $titulo_modal ?></h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     </button>
                 </div>
                 <div class="modal-body">
 
-                    <form method="post" id="content" action="index.php">
+                    <form method="post" id="content" action="cadastros_manager.php">
+                        <input type="hidden" name="view" value="conta">
                         <input type="hidden" name="id" value="">
-                        <label>Informe os dados da empresa </label>
+                        <input type="hidden" name="target" value="<?php echo $_GET['target'] ?? ''; ?>">
+                        <input type="hidden" name="con01id" value="<?php echo $_GET['con01id'] ?? ''; ?>">
 
                         <div class="input-nome input-form-adm">
-                            <!-- Razão social / Nome: -->
-                            <input type="text" onchange="checar()" name="nome" class="form-control" placeholder="Razão social / Nome"
-                                value="" required>
+                            <!--Nome: -->
+                            <label for="nome">Nome:</label>
+                            <input type="text" onchange="checar()" name="nome" class="form-control" placeholder="Nome" value="" required>
                         </div>
-
-                        
-
-                        <div class="input-fantasia input-form-adm">
-                            <!--Nome fantasia-->
-                            <input type="text" onchange="checar()" name="fantasia"
-                                class="form-control" placeholder="Nome fantasia" value="" required>
+                    <?php if(isset($target) && $target == 'titulo'){ ?>
+                        <div class="input-nome input-form-adm">
+                            <label for="nome">Tipo:</label>
+                            <select name="tipo" class="form-select" id="tipo">
+                                <option value="C">Crédito</option>
+                                <option value="D">Débito</option>
+                            </select>
                         </div>
+                    <?php } ?>
 
-                        
-
-                        <div class="input-cpf input-form-adm">
-                            <!--CPF-->
-                            <input type="text" onchange="checar()" name="cpf" class="form-control"
-                                placeholder="CPF" value="" required>
-                        </div>
-
-                        
-
-                        <div class="input-cnpj input-form-adm">
-                            <!--cnpj-->
-                            <input type="text" onchange="checar()" name="cnpj" class="form-control"
-                                placeholder="CNPJ" value="" required>
-                        </div>
-
-                        
-                        
-                        <div class="input-cep input-form-adm">
-                            <input type="text" onchange="checar()" name="cep"
-                                class="form-control" placeholder="CEP" value="" required>
-                        </div>
-                        
-                        
-
-                        <div class="input-endereco input-form-adm">
-                            <input type="text" onchange="checar()" name="endereco" class="form-control"
-                                placeholder="Endereço" value="" required>
-                        </div>
-
-                        <div class="input-form-adm-group input-form-adm">
-                                <div class="input-bairro">
-                                    <input type="text" onchange="checar()" name="bairro" class="form-control" style="border-top-right-radius: 0; border-bottom-right-radius: 0;"
-                                        placeholder="Bairro" value="" required>
-                                </div>
-
-                                
-
-                                <div class="input-cidade">
-                                    <input type="text" onchange="checar()" name="cidade" class="form-control" style=" border-radius: 0;"
-                                        placeholder="Cidade" value="" required>
-                                </div>
-
-                                
-
-                                <div class="input-estado">
-                                    <input type="text" onchange="checar()" name="estado" class="form-control" style="border-top-left-radius: 0; border-bottom-left-radius: 0;"
-                                        placeholder="Estado" value="" required>
-                                </div>
-                        </div>
-                        
-
-                        <div class="input-form-contato-adm-group input-form-adm">
-
-                        
-
-                        <div class="input-celular">
-                            <input type="text" onchange="checar()" name="celular" class="form-control" style="border-top-right-radius: 0; border-bottom-right-radius: 0;"
-                                placeholder="Celular" value="" required>
-                        </div>
-
-                        
-
-                        <div class="input-telefone">
-                            <input type="text" onchange="checar()" name="telefone" class="form-control" style="border-top-left-radius: 0; border-bottom-left-radius: 0;"
-                                placeholder="Telefone Fixo" value="" required>
-                        </div>
-                        
-                        </div>
-
-                        <div class="input-email input-form-adm">
-                            <input type="text" onchange="checar()" name="email" class="form-control"
-                                placeholder="E-mail" value="" required>
-                        </div>
-
-                        
-
-                        <div style="margin-left:1.25em; margin-top:0; align-self:center;" class="input-status input-form-adm">
-                            <label for="status" style="margin-bottom:0;">Ativo</label>
-                            <input type="checkbox" checked onchange="" name="status" class="form-check-input"
-                                 value="">
-                        </div>
-
-                        
 
                         <div style="margin-bottom: 3em;" class="footer">
 
-                        <button name="acao" value="adicionar" class="btn btn-success" style="background-color: #5856d6; border: #5856d6; border-top-right-radius: 0; border-bottom-right-radius: 0;" disabled href="consulta_cliente.php">Salvar</button>
+                        <button name="acao" value="adicionar" class="btn btn-success" style="background-color: #5856d6; border: #5856d6; border-top-right-radius: 0; border-bottom-right-radius: 0;" href="consulta_cliente.php">Salvar</button>
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="border-top-left-radius: 0; border-bottom-left-radius: 0;">Fechar</button>
                             
 
@@ -298,29 +286,29 @@ document.addEventListener('DOMContentLoaded', function() {
         
 
 
-    function checar() {
-        let nome = document.querySelector('.input-nome input').value;
-        let fantasia = document.querySelector('.input-fantasia input').value;
-        let cpf = document.querySelector('.input-cpf input').value;
-        let cnpj = document.querySelector('.input-cnpj input').value;
-        let cep = document.querySelector('.input-cep input').value;
-        let endereco = document.querySelector('.input-endereco input').value;
-        let bairro = document.querySelector('.input-bairro input').value;
-        let cidade = document.querySelector('.input-cidade input').value;
-        let estado = document.querySelector('.input-estado input').value;
-        let celular = document.querySelector('.input-celular input').value;
-        let telefone = document.querySelector('.input-telefone input').value;
-        let email = document.querySelector('.input-email input').value;
+    // function checar() {
+    //     let nome = document.querySelector('.input-nome input').value;
+    //     let fantasia = document.querySelector('.input-fantasia input').value;
+    //     let cpf = document.querySelector('.input-cpf input').value;
+    //     let cnpj = document.querySelector('.input-cnpj input').value;
+    //     let cep = document.querySelector('.input-cep input').value;
+    //     let endereco = document.querySelector('.input-endereco input').value;
+    //     let bairro = document.querySelector('.input-bairro input').value;
+    //     let cidade = document.querySelector('.input-cidade input').value;
+    //     let estado = document.querySelector('.input-estado input').value;
+    //     let celular = document.querySelector('.input-celular input').value;
+    //     let telefone = document.querySelector('.input-telefone input').value;
+    //     let email = document.querySelector('.input-email input').value;
 
 
 
 
-        if (nome !== '' && fantasia !== '' && cpf !== '' && cnpj !== '' && cep !== '' && endereco !== '' && bairro !== '' && cidade !== '' && estado !== '' && celular !== '' && telefone !== '' && email !== '') {
-            document.querySelector('button[name="acao"]').disabled = false;
-        } else {
-            document.querySelector('button[name="acao"]').disabled = true;
-        }
-    }
+    //     if (nome !== '' && fantasia !== '' && cpf !== '' && cnpj !== '' && cep !== '' && endereco !== '' && bairro !== '' && cidade !== '' && estado !== '' && celular !== '' && telefone !== '' && email !== '') {
+    //         document.querySelector('button[name="acao"]').disabled = false;
+    //     } else {
+    //         document.querySelector('button[name="acao"]').disabled = true;
+    //     }
+    // }
 
     function encolher() {
         let barra = document.getElementById('barra-lateral');
@@ -374,11 +362,11 @@ if (barra.style.animationName === 'encolher') {
 <?php if ( isset($_GET['acao']) && $_GET['acao'] == 'adicionar') { ?>
     <script>
         window.addEventListener('DOMContentLoaded', function () {
-            var modalEl = document.getElementById('modal_empresa');
+            var modalEl = document.getElementById('modal_titulo');
             var Modal = new bootstrap.Modal(modalEl);
             Modal.show();
             modalEl.addEventListener('hidden.bs.modal', function () {
-                window.location.href = 'index.php';
+                window.location.href = 'index.php?view=<?= $_GET['view'] ?>';
             });
         });
 
