fb7af37764f30a9430ec0a5db13e5fe927303650
Thiago <thiagotejadafabres@gmail.com>
2025-09-02 18:02:24 -0300

    crud de contas a pagar e contas a receber adicionado

---
+++ db/entities/pagar.php
@@ -0,0 +1,220 @@
+<?php
+
+require_once '../db/base.php';
+
+class Pag01 {
+    public $id;
+    public $id_empresa;
+    public $id_cadastro;
+    public $id_con01;
+    public $id_con02;
+    public $documento;
+    public $descricao;
+    public $valor;
+    public $parcelas;
+    public $data_lanc;
+    public $id_usuario;
+    public function __construct($id = null, $id_empresa = null, $id_cadastro = null, $id_con01 = null, $id_con02 = null, $documento = '', $descricao = '', $valor = 0.0, $parcelas = 1, $data_lanc = null, $id_usuario = null, $obs = null) {
+        $this->id = $id;
+        $this->id_empresa = $id_empresa;
+        $this->id_cadastro = $id_cadastro;
+        $this->id_con01 = $id_con01;
+        $this->id_con02 = $id_con02;
+        $this->documento = $documento;
+        $this->descricao = $descricao;
+        $this->valor = $valor;
+        $this->parcelas = $parcelas;
+        $this->data_lanc = $data_lanc ? new DateTime($data_lanc) : new DateTime();
+        $this->id_usuario = $id_usuario;
+    }
+
+    public static function create($pag01) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'INSERT INTO pag01 (id_empresa, id_cadastro, id_con01, id_con02, documento, descricao, valor, parcelas, data_lanc, id_usuario) 
+                VALUES (:id_empresa, :id_cadastro, :id_con01, :id_con02, :documento, :descricao, :valor, :parcelas, :data_lanc, :id_usuario)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id_empresa', $pag01->id_empresa);
+        $stmt->bindValue(':id_cadastro', $pag01->id_cadastro);
+        $stmt->bindValue(':id_con01', $pag01->id_con01);
+        $stmt->bindValue(':id_con02', $pag01->id_con02);
+        $stmt->bindValue(':documento', $pag01->documento);
+        $stmt->bindValue(':descricao', $pag01->descricao);
+        $stmt->bindValue(':valor', $pag01->valor);
+        $stmt->bindValue(':parcelas', $pag01->parcelas);
+        $stmt->bindValue(':data_lanc', $pag01->data_lanc->format('Y-m-d H:i:s'));
+        $stmt->bindValue(':id_usuario', $pag01->id_usuario);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function read($id = null, $id_empresa = null, $id_cadastro = null, $documento = null): array {
+        $pdo = (new Database())->connect();
+        $query = 'SELECT * FROM pag01';
+        $conditions = [];
+
+        if ($id != null) $conditions[] = 'id = :id';
+        if ($id_empresa != null) $conditions[] = 'id_empresa = :id_empresa';
+        if ($id_cadastro != null) $conditions[] = 'id_cadastro = :id_cadastro';
+        if ($documento != null) $conditions[] = 'documento = :documento';
+
+        if ($conditions) {
+            $query .= ' WHERE ' . implode(' AND ', $conditions);
+        }
+
+        $stmt = $pdo->prepare($query);
+
+        if ($id != null) $stmt->bindValue(':id', $id);
+        if ($id_empresa != null) $stmt->bindValue(':id_empresa', $id_empresa);
+        if ($id_cadastro != null) $stmt->bindValue(':id_cadastro', $id_cadastro);
+        if ($documento != null) $stmt->bindValue(':documento', $documento);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, self::class);
+    }
+
+    public static function update($pag01) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'UPDATE pag01 
+                SET id_cadastro = :id_cadastro, id_con01 = :id_con01, id_con02 = :id_con02, documento = :documento, descricao = :descricao, valor = :valor, parcelas = :parcelas, data_lanc = :data_lanc, id_usuario = :id_usuario 
+                WHERE id = :id';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $pag01->id);
+        $stmt->bindValue(':id_cadastro', $pag01->id_cadastro);
+        $stmt->bindValue(':id_con01', $pag01->id_con01);
+        $stmt->bindValue(':id_con02', $pag01->id_con02);
+        $stmt->bindValue(':documento', $pag01->documento);
+        $stmt->bindValue(':descricao', $pag01->descricao);
+        $stmt->bindValue(':valor', $pag01->valor);
+        $stmt->bindValue(':parcelas', $pag01->parcelas);
+        $stmt->bindValue(':data_lanc', $pag01->data_lanc->format('Y-m-d H:i:s'));
+        $stmt->bindValue(':id_usuario', $pag01->id_usuario);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function delete($id) {
+        $pdo = (new Database())->connect();
+        $sql = 'DELETE FROM pag01 WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+}
+}
+
+//parcelas
+class Pag02 {
+    public $id;
+    public $id_empresa;
+    public $id_pag01;
+    public $valor_par;
+    public $parcela;
+    public $vencimento;
+    public $valor_pag;
+    public $data_pag;
+    public $obs;
+    public $id_pgto;
+
+    public function __construct($id = null, $id_empresa = null, $id_pag01 = null, $valor_par = 0.0, $parcela = 1, $vencimento = null, $valor_pag = 0.0, $data_pag = null, $obs = '', $id_pgto = null) {
+        $this->id = $id;
+        $this->id_empresa = $id_empresa;
+        $this->id_pag01 = $id_pag01;
+        $this->valor_par = $valor_par;
+        $this->parcela = $parcela;
+        $this->vencimento = $vencimento;
+        $this->valor_pag = $valor_pag;
+        $this->data_pag = $data_pag ? new DateTime($data_pag) : null;
+        $this->obs = $obs;
+        $this->id_pgto = $id_pgto;
+    }
+
+    public static function create($pag02) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'INSERT INTO pag02 (id_empresa, id_pag01, valor_par, parcela, vencimento, valor_pag, data_pag, obs, id_pgto) 
+                VALUES (:id_empresa, :id_pag01, :valor_par, :parcela, :vencimento, :valor_pag, :data_pag, :obs, :id_pgto)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id_empresa', $pag02->id_empresa);
+        $stmt->bindValue(':id_pag01', $pag02->id_pag01);
+        $stmt->bindValue(':valor_par', $pag02->valor_par);
+        $stmt->bindValue(':parcela', $pag02->parcela);
+        $stmt->bindValue(':vencimento', $pag02->vencimento);
+        $stmt->bindValue(':valor_pag', $pag02->valor_pag);
+        $stmt->bindValue(':data_pag', $pag02->data_pag ? $pag02->data_pag->format('Y-m-d') : null);
+        $stmt->bindValue(':obs', $pag02->obs);
+        $stmt->bindValue(':id_pgto', $pag02->id_pgto);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function read($id = null, $id_empresa = null, $id_pag01 = null, $data = null, $parcela = null): array {
+        $pdo = (new Database())->connect();
+        $query = 'SELECT * FROM pag02';
+        $conditions = [];
+
+        if ($id != null) $conditions[] = 'id = :id';
+        if ($id_empresa != null) $conditions[] = 'id_empresa = :id_empresa';
+        if ($id_pag01 != null) $conditions[] = 'id_pag01 = :id_pag01';
+        if ($data != null) $conditions[] = 'MONTH(vencimento) = MONTH(:data) AND YEAR(vencimento) = YEAR(:data)';
+        if ($parcela != null) $conditions[] = 'parcela = :parcela';
+        if ($conditions) {
+            $query .= ' WHERE ' . implode(' AND ', $conditions);
+        }
+
+        $stmt = $pdo->prepare($query);
+
+        if ($id != null) $stmt->bindValue(':id', $id);
+        if ($id_empresa != null) $stmt->bindValue(':id_empresa', $id_empresa);
+        if ($id_pag01 != null) $stmt->bindValue(':id_pag01', $id_pag01);
+        if ($parcela != null) $stmt->bindValue(':parcela', $parcela);
+        if ($data != null) {
+        if ($data instanceof DateTime) {
+            $data = $data->format('Y-m-d'); // ou 'Y-m' se só quiser comparar mês
+        }
+        $stmt->bindValue(':data', $data);
+    }
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, self::class);
+    }
+
+    public static function update($pag02) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'UPDATE pag02 
+                SET valor_par = :valor_par, parcela = :parcela, vencimento = :vencimento, valor_pag = :valor_pag, data_pag = :data_pag, obs = :obs, id_pgto = :id_pgto 
+                WHERE id = :id';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $pag02->id);
+        $stmt->bindValue(':valor_par', $pag02->valor_par);
+        $stmt->bindValue(':parcela', $pag02->parcela);
+        $stmt->bindValue(':vencimento', $pag02->vencimento);
+        $stmt->bindValue(':valor_pag', $pag02->valor_pag);
+        $stmt->bindValue(':data_pag', $pag02->data_pag ? $pag02->data_pag->format('Y-m-d') : null);
+        $stmt->bindValue(':obs', $pag02->obs);
+        $stmt->bindValue(':id_pgto', $pag02->id_pgto);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function delete($id) {
+        $pdo = (new Database())->connect();
+        $sql = 'DELETE FROM pag02 WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+}
+
+}
+?>
---
+++ db/entities/recebimentos.php
@@ -0,0 +1,220 @@
+<?php
+
+require_once '../db/base.php';
+
+class Rec01 {
+    public $id;
+    public $id_empresa;
+    public $id_cadastro;
+    public $id_con01;
+    public $id_con02;
+    public $documento;
+    public $descricao;
+    public $valor;
+    public $parcelas;
+    public $data_lanc;
+    public $id_usuario;
+    public function __construct($id = null, $id_empresa = null, $id_cadastro = null, $id_con01 = null, $id_con02 = null, $documento = '', $descricao = '', $valor = 0.0, $parcelas = 1, $data_lanc = null, $id_usuario = null, $obs = null) {
+        $this->id = $id;
+        $this->id_empresa = $id_empresa;
+        $this->id_cadastro = $id_cadastro;
+        $this->id_con01 = $id_con01;
+        $this->id_con02 = $id_con02;
+        $this->documento = $documento;
+        $this->descricao = $descricao;
+        $this->valor = $valor;
+        $this->parcelas = $parcelas;
+        $this->data_lanc = $data_lanc ? new DateTime($data_lanc) : new DateTime();
+        $this->id_usuario = $id_usuario;
+    }
+
+    public static function create($rec01) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'INSERT INTO rec01 (id_empresa, id_cadastro, id_con01, id_con02, documento, descricao, valor, parcelas, data_lanc, id_usuario) 
+                VALUES (:id_empresa, :id_cadastro, :id_con01, :id_con02, :documento, :descricao, :valor, :parcelas, :data_lanc, :id_usuario)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id_empresa', $rec01->id_empresa);
+        $stmt->bindValue(':id_cadastro', $rec01->id_cadastro);
+        $stmt->bindValue(':id_con01', $rec01->id_con01);
+        $stmt->bindValue(':id_con02', $rec01->id_con02);
+        $stmt->bindValue(':documento', $rec01->documento);
+        $stmt->bindValue(':descricao', $rec01->descricao);
+        $stmt->bindValue(':valor', $rec01->valor);
+        $stmt->bindValue(':parcelas', $rec01->parcelas);
+        $stmt->bindValue(':data_lanc', $rec01->data_lanc->format('Y-m-d H:i:s'));
+        $stmt->bindValue(':id_usuario', $rec01->id_usuario);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function read($id = null, $id_empresa = null, $id_cadastro = null, $documento = null): array {
+        $pdo = (new Database())->connect();
+        $query = 'SELECT * FROM rec01';
+        $conditions = [];
+
+        if ($id != null) $conditions[] = 'id = :id';
+        if ($id_empresa != null) $conditions[] = 'id_empresa = :id_empresa';
+        if ($id_cadastro != null) $conditions[] = 'id_cadastro = :id_cadastro';
+        if ($documento != null) $conditions[] = 'documento = :documento';
+
+        if ($conditions) {
+            $query .= ' WHERE ' . implode(' AND ', $conditions);
+        }
+
+        $stmt = $pdo->prepare($query);
+
+        if ($id != null) $stmt->bindValue(':id', $id);
+        if ($id_empresa != null) $stmt->bindValue(':id_empresa', $id_empresa);
+        if ($id_cadastro != null) $stmt->bindValue(':id_cadastro', $id_cadastro);
+        if ($documento != null) $stmt->bindValue(':documento', $documento);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, self::class);
+    }
+
+    public static function update($rec01) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'UPDATE rec01 
+                SET id_cadastro = :id_cadastro, id_con01 = :id_con01, id_con02 = :id_con02, documento = :documento, descricao = :descricao, valor = :valor, parcelas = :parcelas, data_lanc = :data_lanc, id_usuario = :id_usuario 
+                WHERE id = :id';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $rec01->id);
+        $stmt->bindValue(':id_cadastro', $rec01->id_cadastro);
+        $stmt->bindValue(':id_con01', $rec01->id_con01);
+        $stmt->bindValue(':id_con02', $rec01->id_con02);
+        $stmt->bindValue(':documento', $rec01->documento);
+        $stmt->bindValue(':descricao', $rec01->descricao);
+        $stmt->bindValue(':valor', $rec01->valor);
+        $stmt->bindValue(':parcelas', $rec01->parcelas);
+        $stmt->bindValue(':data_lanc', $rec01->data_lanc->format('Y-m-d H:i:s'));
+        $stmt->bindValue(':id_usuario', $rec01->id_usuario);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function delete($id) {
+        $pdo = (new Database())->connect();
+        $sql = 'DELETE FROM rec01 WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+}
+}
+
+//parcelas
+class Rec02 {
+    public $id;
+    public $id_empresa;
+    public $id_rec01;
+    public $valor_par;
+    public $parcela;
+    public $vencimento;
+    public $valor_pag;
+    public $data_pag;
+    public $obs;
+    public $id_pgto;
+
+    public function __construct($id = null, $id_empresa = null, $id_rec01 = null, $valor_par = 0.0, $parcela = 1, $vencimento = null, $valor_pag = 0.0, $data_pag = null, $obs = '', $id_pgto = null) {
+        $this->id = $id;
+        $this->id_empresa = $id_empresa;
+        $this->id_rec01 = $id_rec01;
+        $this->valor_par = $valor_par;
+        $this->parcela = $parcela;
+        $this->vencimento = $vencimento;
+        $this->valor_pag = $valor_pag;
+        $this->data_pag = $data_pag ? new DateTime($data_pag) : null;
+        $this->obs = $obs;
+        $this->id_pgto = $id_pgto;
+    }
+
+    public static function create($rec02) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'INSERT INTO rec02 (id_empresa, id_rec01, valor_par, parcela, vencimento, valor_pag, data_pag, obs, id_pgto) 
+                VALUES (:id_empresa, :id_rec01, :valor_par, :parcela, :vencimento, :valor_pag, :data_pag, :obs, :id_pgto)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id_empresa', $rec02->id_empresa);
+        $stmt->bindValue(':id_rec01', $rec02->id_rec01);
+        $stmt->bindValue(':valor_par', $rec02->valor_par);
+        $stmt->bindValue(':parcela', $rec02->parcela);
+        $stmt->bindValue(':vencimento', $rec02->vencimento);
+        $stmt->bindValue(':valor_pag', $rec02->valor_pag);
+        $stmt->bindValue(':data_pag', $rec02->data_pag ? $rec02->data_pag->format('Y-m-d') : null);
+        $stmt->bindValue(':obs', $rec02->obs);
+        $stmt->bindValue(':id_pgto', $rec02->id_pgto);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function read($id = null, $id_empresa = null, $id_rec01 = null, $data = null, $parcela = null): array {
+        $pdo = (new Database())->connect();
+        $query = 'SELECT * FROM rec02';
+        $conditions = [];
+
+        if ($id != null) $conditions[] = 'id = :id';
+        if ($id_empresa != null) $conditions[] = 'id_empresa = :id_empresa';
+        if ($id_rec01 != null) $conditions[] = 'id_rec01 = :id_rec01';
+        if ($data != null) $conditions[] = 'MONTH(vencimento) = MONTH(:data) AND YEAR(vencimento) = YEAR(:data)';
+        if ($parcela != null) $conditions[] = 'parcela = :parcela';
+        if ($conditions) {
+            $query .= ' WHERE ' . implode(' AND ', $conditions);
+        }
+
+        $stmt = $pdo->prepare($query);
+
+        if ($id != null) $stmt->bindValue(':id', $id);
+        if ($id_empresa != null) $stmt->bindValue(':id_empresa', $id_empresa);
+        if ($id_rec01 != null) $stmt->bindValue(':id_rec01', $id_rec01);
+        if ($parcela != null) $stmt->bindValue(':parcela', $parcela);
+        if ($data != null) {
+        if ($data instanceof DateTime) {
+            $data = $data->format('Y-m-d'); // ou 'Y-m' se só quiser comparar mês
+        }
+        $stmt->bindValue(':data', $data);
+    }
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, self::class);
+    }
+
+    public static function update($rec02) {
+        $pdo = (new Database())->connect();
+
+        $sql = 'UPDATE rec02 
+                SET valor_par = :valor_par, parcela = :parcela, vencimento = :vencimento, valor_pag = :valor_pag, data_pag = :data_pag, obs = :obs, id_pgto = :id_pgto 
+                WHERE id = :id';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $rec02->id);
+        $stmt->bindValue(':valor_par', $rec02->valor_par);
+        $stmt->bindValue(':parcela', $rec02->parcela);
+        $stmt->bindValue(':vencimento', $rec02->vencimento);
+        $stmt->bindValue(':valor_pag', $rec02->valor_pag);
+        $stmt->bindValue(':data_pag', $rec02->data_pag ? $rec02->data_pag->format('Y-m-d') : null);
+        $stmt->bindValue(':obs', $rec02->obs);
+        $stmt->bindValue(':id_pgto', $rec02->id_pgto);
+
+        
+
+        return $stmt->execute();
+    }
+
+    public static function delete($id) {
+        $pdo = (new Database())->connect();
+        $sql = 'DELETE FROM rec02 WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':id', $id);
+}
+
+}
+?>
