3bc14fce06e5accd85b1a924ba2c7024806b0f83
Thiago <thiagotejadafabres@gmail.com>
2025-11-21 13:34:33 -0300

    iniciando a criacao da parte bancaria

---
+++ usuario/bancario/contas/conta_manager.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once __DIR__ . '/../../../db/entities/usuarios.php';
+session_start();
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario']->cargo != 3) {
+    header('Location: /');
+    exit;
+}
+require_once __DIR__ . '/../../../db/entities/banco01.php';
+
+
+echo '<pre>';
+print_r($_POST); 
+echo '</pre>';
+
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$codigo = filter_input(INPUT_POST, 'codigo', FILTER_SANITIZE_NUMBER_INT);
+$agencia = filter_input(INPUT_POST, 'agencia', FILTER_SANITIZE_NUMBER_INT);
+$numero_conta = filter_input(INPUT_POST, 'numero_conta', FILTER_SANITIZE_STRING);
+$acao = filter_input(INPUT_POST, 'acao', FILTER_SANITIZE_STRING);
+
+if($acao == 'adicionar') {
+    $nova_conta = new Ban01(
+        null,
+        $_SESSION['usuario']->id_empresa,
+        $codigo,
+        $agencia,
+        $numero_conta,
+        $nome
+    );
+    $resultado = Ban01::create($nova_conta);
+    if ($resultado) {
+        header('Location: /usuario/bancario/contas/conta.php?sucesso=sucesso.');
+    } else {
+        header('Location: /usuario/bancario/contas/conta.php?erro=erro');
+    }
+}
+else if($acao == 'editar') {
+    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+    $conta_existente = new Ban01(
+        $id,
+        $_SESSION['usuario']->id_empresa,
+        $codigo,
+        $agencia,
+        $numero_conta,
+        $nome
+    );
+    $resultado = Ban01::update($conta_existente);
+    if ($resultado) {
+        header('Location: /usuario/bancario/contas/conta.php?sucesso=sucesso.');
+    } else {
+        header('Location: /usuario/bancario/contas/conta.php?erro=erro');
+    }
+} else {
+    header('Location: /usuario/bancario/contas.php?erro=acao_invalida');
+}
+
+
+?>
