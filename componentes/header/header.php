3dca66caec422533d52cad1fde76e654e33a8c12
Thiago <thiagotejadafabres@gmail.com>
2026-01-12 09:12:32 -0300

    modo de tela cheia atualizado

--- componentes/header/header.php
+++ componentes/header/header.php
@@ -3,13 +3,14 @@ require_once __DIR__ . '/../../db/entities/empresas.php';
 $nomeEmpresa = Empresa::read($_SESSION['usuario']->id_empresa)[0]->nom_fant;
 ?>
 <link rel="stylesheet" href="/componentes/header/header.css"> 
+<link rel="stylesheet" href="/usuario/style/responsivo.css"> 
 
 <div id="header">
        <div id="titulo-header">
             <button class="btn mb-0" onclick="encolher()">
                 <span class="btn bi bi-list mb-0"></span>
             </button>
-            <a href="/usuario/index.php">Dashboard</a>
+            <a>Dashboard</a>
         </div>
 
         <div id="nome-empresa">
@@ -27,3 +28,67 @@ $nomeEmpresa = Empresa::read($_SESSION['usuario']->id_empresa)[0]->nom_fant;
             </div>
         <!-- </div> -->
     </div>
+
+    <script>
+
+
+
+        
+        
+        function encolher(acao) {
+        const barra = document.getElementById('barra-lateral');
+        const container = document.getElementById('container');
+        const superior = document.getElementById('header');
+
+        
+
+
+
+        if ( barra.style.animationName != 'encolher-lateral'){
+            
+
+            if(!document.querySelector('body').clientWidth <= 800) {                
+            localStorage.setItem('tela', 'cheia')
+            superior.style.animationName = 'encolher-header'
+            superior.style.animationDuration = '0.5s';
+            superior.style.animationFillMode = 'forwards';
+
+            container.style.animationName = 'encolher-container'
+            container.style.animationDuration = '0.5s';
+            container.style.animationFillMode = 'forwards';
+            }
+            barra.style.animationName = 'encolher-lateral';
+            barra.style.animationDuration = '0.5s';
+            barra.style.animationFillMode = 'forwards';
+        } else if( barra.style.animationName == 'encolher-lateral') {
+            if(!document.querySelector('body').clientWidth <= 800) {
+                localStorage.setItem('tela', 'normal')
+            superior.style.animationName = 'expandir-header'
+            superior.style.animationDuration = '0.5s';
+            superior.style.animationFillMode = 'backwards';
+            container.style.animationName = 'expandir-container'
+            container.style.animationDuration = '0.5s';
+            container.style.animationFillMode = 'backwards';
+            }
+            barra.style.animationName = 'expandir-lateral';
+            barra.style.animationDuration = '0.5s';
+            barra.style.animationFillMode = 'backwards';
+
+            
+
+            return;
+        } 
+    }
+    if(!document.querySelector('body').clientWidth <= 800) {
+        if(localStorage.getItem('tela') == 'cheia') {
+        setTimeout(
+            () => {
+                encolher('encolher')
+            }, 100
+        )
+        
+    }
+    }
+     
+    
+    </script>
