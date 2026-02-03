ee9490f7265a12a16a479da42e58eff04b46dc6e
Thiago <thiagotejadafabres@gmail.com>
2026-02-03 19:55:26 -0300

    footer adicionado, com o botão de fale conosco

---
+++ componentes/footer/footer.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="/componentes/footer/footer.css">
+<div id="footer">
+    <button type="button" data-bs-toggle="modal" data-bs-target="#modal_fale_conosco" id="footer-btn" > <i class="bi bi-telephone"></i> Fale Conosco</button>
+</div>
+<?php require_once __DIR__ . '/footer_modal.php' ?>
---
+++ componentes/footer/footer_modal.php
@@ -0,0 +1,7 @@
+<div class="modal fade" id="modal_fale_conosco" tabindex="-1" role="dialog" aria-labelledby="modalFaleConoscoLabel">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <img src="/componentes/footer/fale_conosco.png" alt="(27) 99599-0024 contato@gestoroffice.com.br" style="width:100%; height:auto;">
+            </div>
+        </div>
+    </div>
