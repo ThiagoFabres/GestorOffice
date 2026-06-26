3571cc4ff7f2d0d0ec0ce7e0c6dec7d688c6005d
Thiago <thiagotejadafabres@gmail.com>
2026-06-26 16:15:40 -0300

    erro corrigido

--- componentes/modais/lancamentos/pagar/modal_receber.php
+++ componentes/modais/lancamentos/pagar/modal_receber.php
@@ -590,7 +590,7 @@
             faltando.push('Descrição');
         }
 
-        msg.textContent = '⚠ Preencha: ' + faltando.join(', ');
+        msg.textContent = faltando.join(', ');
 
         if (faltando.length === 0) {
             botao.disabled = false;
